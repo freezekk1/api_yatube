694f397ae6ffabaeef91e438e77a08c8a3235784
freezekk1 <shirinkinegor2007@mail.ru>
2026-05-20 15:17:45 +0300

    ф

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,38 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+router.register('groups', GroupViewSet, basename='groups')
+router.register('follow', FollowViewSet, basename='follow')
+
+urlpatterns = [
+    path(
+        'v1/posts/<int:post_id>/comments/',
+        CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='comment-list',
+    ),
+    path(
+        'v1/posts/<int:post_id>/comments/<int:pk>/',
+        CommentViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'put': 'update',
+                'patch': 'partial_update',
+                'delete': 'destroy',
+            }
+        ),
+        name='comment-detail',
+    ),
+    path('v1/jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
+    path('v1/', include(router.urls)),
+]
